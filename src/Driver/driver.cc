e0100a010f3787fc830d7bea6d48d6c61e82ddea
Marlon Brenes <brenesnm@tcd.ie>
2024-10-22 21:13:18 -0400

    Effective non-commuting NESS current driver

--- src/Driver/driver.cc
+++ src/Driver/driver.cc
@@ -1,13 +1,12 @@
 // Redfield calculations
 /*******************************************************************************/
-// Computes the heat current in the two-terminal set-up
-// Double qubit configuration, one coupled to hot the other to cold
-// Phonon mapping with sigma x
-// Reaction coordinate at strong coupling
+// NESS heat current
+// Spin-boson model with two reservoirs
+// Reservoirs at different temperature
+// Testing for effective model with non-commuting operators
 /*******************************************************************************/
 
 #include <iostream>
-#include <math.h>
 #include <numeric>
 #include <algorithm>
 #include <complex>
@@ -16,8 +15,8 @@
 
 #include "../Utils/Utils.h"
 #include "../Redfield/Redfield.h"
+#include "../RungeKutta/RK4.h"
 
-// To evaluate the time a routine takes
 double seconds()
 {
   struct timeval tmp;
@@ -27,7 +26,8 @@ double seconds()
   return sec;
 }
 
-std::vector<double> linspace(double a, double b, size_t n) {
+std::vector<double> linspace(double a, double b, size_t n)
+{
   double h = (b - a) / static_cast<double>(n - 1);
   std::vector<double> vec(n);
   typename std::vector<double>::iterator x;
@@ -37,128 +37,135 @@ std::vector<double> linspace(double a, double b, size_t n) {
   return vec;
 }
 
-// Spectral function of the residual baths
+double dawson_int_trap(double x)
+{
+  int num_disc = 1000;
+  double h = x / num_disc;
+  double f_a = 1.0;
+  double f_b = std::exp(x * x);
+  double accum_odd = 0.0;
+  double accum_even = 0.0;
+  for(int k = 1; k < num_disc; k += 2){
+    accum_odd += std::exp(k * h * k * h);
+  }
+  for(int k = 2; k < num_disc; k += 2){
+    accum_even += std::exp(k * h * k * h);
+  }
+
+  double val = std::exp(-1.0 * x * x) * ((f_b + (4.0 * accum_odd) + (2.0 * accum_even)) * h / 3.0);
+
+  return val;
+}
+
 double ohmic_spectrum(double w, double t)
 {
-  double ga = 0.005; // Coupling to the residual reservoir
+  double ga = 0.05; // Coupling to the residual reservoir
   double wc = 1000.0; // Cut-off frequency
   double aw = std::abs(w);
   double n_b = 1.0 / (std::exp(aw / t) - (1.0 * (w != 0)));
   double j_s = ga * aw * std::exp(-1.0 * aw / wc);
-  
   return (j_s * (n_b + 1.0) * (w > 0)) + (j_s * n_b * (w < 0)) + (ga * t * (w == 0.0));
 }
 
 int main(int argc, char **argv)
 {
   MKL_INT rc = 777; // Local dimension of the reaction cordinates
-  double g = 0.777; // Internal coupling between two qubits
-  double la = 0.777; // System-Reservoir coupling
-  double t_h = 0.777; // Temperature right
-  double t_c = 0.1; // Temperature left
-  double om_h = 15.0; double om_c = 15.0; // Self-energy of the RC
+  double eps = 1.0; // Splitting of the spin
+  // z is the bath with sz, x is the bath with sx
+  double om = 8.0; // Self-energy of the reaction coordinates
+  double la_l = 0.777; // Coupling strength of between spin and reaction coordinate left
+  double la_r = 0.777; // Coupling strength of between spin and reaction coordinate right
+  double t_l = 0.777; // Temperature left
+  double t_r = 0.777; // Temperature right
+  double theta_l = 0.777; // Phase of the coupling left
+  double theta_r = 0.777; //Phase of the coupling right
 
   // Parsing arguments
-  if(argc != 9){
-    std::cerr << "Usage: " << argv[0] << " --rc [rc] --g [g] --la [la] --th [t_h]" << std::endl;
+  if(argc != 15){
+    std::cerr << "Usage: " << argv[0] << " --rc [RC] --la_l [la_l] --la_r [la_r] --t_l [t_l] --t_r [t_r] --theta_l [theta_l] --theta_r [theta_r]" << std::endl;
     exit(1);
   }
   for(int i = 0; i < argc; ++i){
     std::string str = argv[i];
     if(str == "--rc") rc = atoi(argv[i + 1]);
-    if(str == "--g") g = atof(argv[i + 1]);
-    if(str == "--la") la = atof(argv[i + 1]);
-    if(str == "--th") t_h = atof(argv[i + 1]);
+    else if(str == "--la_l") la_l = atof(argv[i + 1]);
+    else if(str == "--la_r") la_r = atof(argv[i + 1]);
+    else if(str == "--t_l") t_l = atof(argv[i + 1]);
+    else if(str == "--t_r") t_r = atof(argv[i + 1]);
+    else if(str == "--theta_l") theta_l = atof(argv[i + 1]);
+    else if(str == "--theta_r") theta_r = atof(argv[i + 1]);
     else continue;
   }
-  if(rc == 777 || t_h == 0.777 || g == 0.777 || la == 0.777){
+  if(rc == 777 || la_l == 0.777 || la_r == 0.777 || t_l == 0.777 || t_r == 0.777 || theta_l == 0.777 || theta_r == 0.777){
     std::cerr << "Error setting parameters" << std::endl;
-    std::cerr << "Usage: " << argv[0] << " --rc [rc] --g [g] --la [la] --th [t_h]" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " --rc [RC] --la_l [la_l] --la_r [la_r] --t_l [t_l] --t_r [t_r] --theta_l [theta_l] --theta_r [theta_r]" << std::endl;
     exit(1);
   }
 
-  double eps_h = 1.0; double eps_c = eps_h; // Local term H two-qubit
-  double la_h = la; double la_c = la; // System-Reservoir couplings
   std::cout << std::fixed;
-  std::cout.precision(5);
+  std::cout.precision(4);
   std::cout << "# Parameters:" << std::endl;
-  std::cout << "# eps_h = " << eps_h << " eps_c = " << eps_c << std::endl;
-  std::cout << "# g = " << g << std::endl;
   std::cout << "# RC = " << rc << std::endl;
-  std::cout << "# la_h = " << la_h << " la_c = " << la_c << std::endl;
-  std::cout << "# om_h = " << om_h << " om_c = " << om_c << std::endl;
-  std::cout << "# t_h = " << t_h << " t_c = " << t_c << std::endl;
-  std::cout << "# ga = 0.005" << std::endl;
+  std::cout << "# la_l = " << la_l << " la_r = " << la_r << std::endl;
+  std::cout << "# t_l = " << t_l << " t_r = " << t_r << std::endl;
+  std::cout << "# theta_l = " << theta_l << " theta_r = " << theta_r << std::endl;
 
-  // Hamiltonian. Matrices implemented in Utils.
-  // Careful! Spin matrices in Utils are mutable.
-  // Types defined in Utils: MZType is std::vector< std::complex<double> >
-  // CType is std::complex<double>
-  // Two-qubit Hamiltonian
-  MZType sigma_pm = {Utils::p_one_r, Utils::zero, Utils::zero, Utils::zero};
-  MZType TwoBit = Utils::kron(eps_h, 
-                              sigma_pm,
-                              Utils::iden);
-  MZType t1 = Utils::kron(eps_c, 
-                          Utils::iden,
-                          sigma_pm);
-  MZType t2 = Utils::kron(g,
-                          Utils::sigp,
-                          Utils::sigm);
-  MZType t3 = Utils::kron(g,
-                          Utils::sigm,
-                          Utils::sigp);
-  
+  // Coupling operators
+  CType zero_t(0.0, 0.0);
+  MZType stheta_l = {zero_t, zero_t, zero_t, zero_t};
+  MZType stheta_r = {zero_t, zero_t, zero_t, zero_t};
+  for(MKL_INT i = 0; i < 4; ++i){
+    stheta_l[i] = (std::cos(theta_l) * Utils::sigz[i]) + (std::sin(theta_l) * Utils::sigx[i]);
+    stheta_r[i] = (std::cos(theta_r) * Utils::sigz[i]) + (std::sin(theta_r) * Utils::sigx[i]);
+  }
 
-  MZType n_vec(TwoBit.size(), 0.0);
-  Utils::add(TwoBit, t1, t2);
-  Utils::add(TwoBit, t3, n_vec);
-  // Hamiltonian system + RCs
-  MZType Ham = Utils::kron(1.0,
-                           TwoBit,
-                           Utils::kron(1.0, Utils::eye(rc), Utils::eye(rc)));
-  MZType t4 = Utils::kron(om_h,
-                          Utils::eye(4),
-                          Utils::kron(1.0, Utils::ata(rc), Utils::eye(rc)));
-  MZType t5 = Utils::kron(om_c,
-                          Utils::eye(4),
-                          Utils::kron(1.0, Utils::eye(rc), Utils::ata(rc)));
-  MZType t6 = Utils::kron(la_h,
-                          Utils::kron(1.0, Utils::sigx, Utils::iden),
-                          Utils::kron(1.0, Utils::atpa(rc), Utils::eye(rc)));
-  MZType t7 = Utils::kron(la_c,
-                          Utils::kron(1.0, Utils::iden, Utils::sigx),
-                          Utils::kron(1.0, Utils::eye(rc), Utils::atpa(rc)));
-  
-  Utils::add(Ham, t4, t5);
-  Utils::add(Ham, t6, t7);
+  double fact_1 = 1.0 - ((std::sqrt(2.0) * la_l / om) * dawson_int_trap(std::sqrt(2.0) * la_l / om));
+  double fact_2 = 1.0 - ((2.0 * std::sqrt(2.0) * la_l / om) * dawson_int_trap(std::sqrt(2.0) * la_l / om));
+  MKL_INT size_s = 1 << 1;
+  MZType Ham(size_s * size_s, 0.0);
+  for(MKL_INT i = 0; i < (size_s * size_s); ++i){
+    Ham[i] = eps * std::exp(-4.0 * la_l * la_l / (om * om)) * Utils::sigz[i];
+    //Ham[i] = eps * fact_1 * Utils::sigz[i];
+  }
+  //Ham[0] = Ham[0] - (((2.0 * la_l * la_l) / om) * fact_1 * fact_1);
+  //Ham[3] = Ham[3] - (((2.0 * la_l * la_l) / om) * fact_1 * fact_1);
+  Ham[0] = Ham[0] - ((2.0 * la_l * la_l) / om);
+  Ham[3] = Ham[3] - ((2.0 * la_l * la_l) / om);
 
-  // Interaction Hamiltonian between RCs and residual baths, system OPs
+  MZType SigmaZ = Utils::sigz;
+  MZType null_s(size_s * size_s, 0.0);
+  CType alpha(1.0, 0.0);
+  CType beta(0.0, 0.0);
+
+  // Interaction Hamiltonian between sytem and bath, system OPs
   // Phonon coupling
-  MZType Vh = Utils::kron(1.0,
-                          Utils::eye(4),
-                          Utils::kron(1.0, Utils::atpa(rc), Utils::eye(rc)));
-  MZType Vc = Utils::kron(1.0,
-                          Utils::eye(4),
-                          Utils::kron(1.0, Utils::eye(rc), Utils::atpa(rc)));
-  
+  MZType V_l(size_s * size_s, 0.0);
+  MZType V_r(size_s * size_s, 0.0);
+  for(MKL_INT i = 0; i < (size_s * size_s); ++i){
+    V_l[i] = (-2.0 * la_l / om) * Utils::sigx[i];
+    V_r[i] = (-2.0 * la_r / om) * Utils::sigx[i];
+    //V_l[i] = (-2.0 * la_l / om) * fact_1 * stheta_l[i];
+    //V_r[i] = (-2.0 * la_r / om) * fact_1 * stheta_r[i];
+    //V_l[i] = (-2.0 * la_l / om) * fact_1 * Utils::sigx[i];
+    //V_r[i] = (-2.0 * la_r / om) * fact_1 * Utils::sigy[i];
+  }
+
   // Redfield tensor
   double time1 = seconds();
   MKL_INT ham_dim = static_cast<MKL_INT>(std::sqrt(Ham.size()));
   Redfield redT(ham_dim);
 
-  MZType RedH((ham_dim * ham_dim) * (ham_dim * ham_dim), 0.0);
-  MZType RedC((ham_dim * ham_dim) * (ham_dim * ham_dim), 0.0);
+  MZType Red_l((ham_dim * ham_dim) * (ham_dim * ham_dim), 0.0);
+  MZType Red_r((ham_dim * ham_dim) * (ham_dim * ham_dim), 0.0);
   MZType Coh((ham_dim * ham_dim) * (ham_dim * ham_dim), 0.0);
-  redT.construct_redfield_2r_phonon(Coh, RedH, RedC, Ham, Vh, Vc, &ohmic_spectrum, t_h, t_c);
+  redT.construct_redfield_2r_phonon(Coh, Red_l, Red_r, Ham, V_l, V_r, 
+          &ohmic_spectrum, &ohmic_spectrum, t_l, t_r);
   double time2 = seconds();
 
   std::cout << "# Time Redfield: " << time2 - time1 << std::endl;
   // Write a superoperator onto Coh
-  CType alpha(1.0, 0.0);
-  CType beta(0.0, 0.0);
-  cblas_zaxpy(Coh.size(), &alpha, &RedH[0], 1, &Coh[0], 1);
-  cblas_zaxpy(Coh.size(), &alpha, &RedC[0], 1, &Coh[0], 1);
+  cblas_zaxpy(Coh.size(), &alpha, &Red_l[0], 1, &Coh[0], 1);
+  cblas_zaxpy(Coh.size(), &alpha, &Red_r[0], 1, &Coh[0], 1);
 
   // Compute steady state
   time1 = seconds();
@@ -166,6 +173,7 @@ int main(int argc, char **argv)
   time2 = seconds();
 
   std::cout << "# Time SS solution: " << time2 - time1 << std::endl;
+
   // Compute heat current
   time1 = seconds();
   MZType rho_diss(ham_dim * ham_dim, 0.0);
@@ -174,14 +182,13 @@ int main(int argc, char **argv)
               ham_dim * ham_dim,
               ham_dim * ham_dim,
               &alpha,
-              &RedH[0],
+              &Red_l[0],
               ham_dim * ham_dim,
               &steady_state[0],
               1,
               &beta,
               &rho_diss[0],
               1);
-
   // Trace
   CType h_curr(0.0, 0.0);
   for(MKL_INT i = 0; i < ham_dim; ++i){
@@ -190,31 +197,30 @@ int main(int argc, char **argv)
   time2 = seconds();
 
   std::cout << "# Time Expc value: " << time2 - time1 << std::endl;
-#if 0
-  // Negativity measure
-  // Rotate state back to spin basis
-  MZType buffer(ham_dim * ham_dim, 0.0);
-  cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasTrans, ham_dim, ham_dim, ham_dim, &alpha, &steady_state[0], ham_dim, &Ham[0], ham_dim, &beta, &buffer[0], ham_dim);
-  cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, ham_dim, ham_dim, ham_dim, &alpha, &Ham[0], ham_dim, &buffer[0], ham_dim, &beta, &steady_state[0], ham_dim);
- 
-  // Partial transpose
-  MZType ss_copy = steady_state;
-  ss_copy[1] = steady_state[4]; ss_copy[3] = steady_state[6];
-  ss_copy[4] = steady_state[1]; ss_copy[6] = steady_state[3];
-  ss_copy[9] = steady_state[12]; ss_copy[11] = steady_state[14];
-  ss_copy[12] = steady_state[9]; ss_copy[14] = steady_state[11];
-  // Eigvals
-  std::vector<double> lambdas(ham_dim, 0.0);
-  MKL_INT info = LAPACKE_zheevd(LAPACK_ROW_MAJOR, 'V', 'U', ham_dim, &ss_copy[0], ham_dim, &lambdas[0]);
 
-  double negativity = 0.0;
+#if 0
+  MZType Rho(ham_dim * ham_dim, 0.0);
   for(MKL_INT i = 0; i < ham_dim; ++i){
-    if(lambdas[i] < 0) negativity += std::abs(lambdas[i]);   
+    for(MKL_INT j = 0; j < ham_dim; ++j){
+      Rho[(i * ham_dim) + j] = steady_state[(j * ham_dim) + i];
+    }
   }
+  // Rotate back to tensor basis
+  MZType buffer(ham_dim * ham_dim, 0.0);
+  cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, ham_dim, ham_dim, ham_dim, &alpha,
+              &Ham[0], ham_dim, &Rho[0], ham_dim, &beta, &buffer[0], ham_dim);  
+  cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasConjTrans, ham_dim, ham_dim, ham_dim, &alpha,
+              &buffer[0], ham_dim, &Ham[0], ham_dim, &beta, &Rho[0], ham_dim);
 #endif
   std::cout << std::scientific;
-  std::cout.precision(15);
-  std::cout << g << " " << h_curr.real() << " " << h_curr.imag() << std::endl;
+  std::cout.precision(6);
+  //std::cout << theta_l << " " << theta_r << " " << h_curr.real() << std::endl;
+  std::cout << la_l << " " << la_r << " " << h_curr.real() << std::endl;
+  //CType im_const = {0.0, 1.0};
+  //CType x_val = Rho[1] + Rho[2];
+  //CType y_val = im_const * (Rho[1] - Rho[2]);
+  //CType z_val = Rho[0] - Rho[3];
+  //std::cout << la_l << " " << la_r << " " << x_val.real() << " " << y_val.real() << " " << z_val.real() << std::endl;
 
   return 0;
 }
